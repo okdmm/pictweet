e9dcc674427b6a824baa80ddc5be31e57c0943de
okada <kirinnremonn1122@gmail.com>
2018-04-09 01:59:54 +0900

    編集画面の完成

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   get 'tweets/new' => 'tweets#new'
   post 'tweets' => 'tweets#create'
   delete 'tweets/:id' => 'tweets#destroy'
-  get 'tweets/:id/edit' => 'tweets#edit'
   patch 'tweets/:id' => 'tweets#update'
+  get 'tweets/:id/edit' => 'tweets#edit'
   get 'users/:id' => 'users#show'
 end
