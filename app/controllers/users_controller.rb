b63050a8bda7b757052134d983b7c23d0690d66e
okada <kirinnremonn1122@gmail.com>
2018-04-09 00:12:33 +0900

    アソシエーションの適用

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 
   def show
-    @tweets = Tweet.where(user_id: current_user.id).page(params[:page]).per(5).order("created_at DESC")
+    @tweets = current_user.tweets.page(params[:page]).per(5).order("created_at DESC")
     @nickname =current_user.nickname 
   end
 end
