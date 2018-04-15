ea01aa9b4a7defa907daf14234dc60474f7e0b98
okada <kirinnremonn1122@gmail.com>
2018-04-15 21:51:23 +0900

    テスト

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,6 +1,6 @@
 class TweetsController < ApplicationController
 
-  before_action :move_to_index, except: :index
+ # before_action :move_to_index, except: :index
   def index
     @tweets = Tweet.includes(:user).order("created_at DESC").page(params[:page]).per(5)
   end
